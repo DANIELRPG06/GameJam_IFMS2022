48a99a9ceebe1595d23dcf61fa788a21ee91cffc
Daniel <danielbatatarpg@hotmail.com>
2022-10-04 18:48:11 -0400

    function

--- Assets/Project Assets/Scripts/Interagiveis/Jaula.cs
+++ Assets/Project Assets/Scripts/Interagiveis/Jaula.cs
@@ -3,18 +3,14 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class Jaula: MonoBehaviour, IInteragivel
 {
-    [SerializeField]
-    private Animator jaulaAnim;
-    
     public bool Interagir(Player player)
     {
         if (player.chaveQtd > 0)
         {
             player.DecrementaChave();
             player.DaPontos(100);
-            
-            jaulaAnim.SetTrigger("abrir");
-
+            //TODO animacao e tal
+            Destroy(gameObject);
             return true;
         }
         return false;
