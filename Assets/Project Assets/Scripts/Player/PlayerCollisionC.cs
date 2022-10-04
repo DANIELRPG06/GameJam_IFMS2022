b2ea7849bad133badfb01721c5069d490bd52e33
Daniel <danielbatatarpg@hotmail.com>
2022-10-03 20:10:59 -0400

    Script de coletavel semifeito

--- Assets/Project Assets/Scripts/Player/PlayerCollisionC.cs
+++ Assets/Project Assets/Scripts/Player/PlayerCollisionC.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
 public class PlayerCollisionC : MonoBehaviour
 {
+    
     [SerializeField] private int chaveQtd = 0;
     // Start is called before the first frame update
 
-    
-
-    
+   
 
 
     private void OnTriggerEnter2D(Collider2D collision)
