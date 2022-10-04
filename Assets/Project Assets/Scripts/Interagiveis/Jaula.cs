333d033be12127caddd6be98dd436bb1ce33739e
Eric Lopes <erichhlopes@gmail.com>
2022-10-04 17:27:09 -0400

    animacao jaula

--- Assets/Project Assets/Scripts/Interagiveis/Jaula.cs
+++ Assets/Project Assets/Scripts/Interagiveis/Jaula.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class Jaula: MonoBehaviour, IInteragivel
 {
+    [SerializeField]
+    private Animator jaulaAnim;
+    
     public bool Interagir(Player player)
     {
         if (player.chaveQtd > 0)
         {
             player.DecrementaChave();
             player.DaPontos(100);
-            //TODO animacao e tal
-            Destroy(gameObject);
+            
+            jaulaAnim.SetTrigger("abrir");
+
             return true;
         }
         return false;
