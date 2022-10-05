7e4be5778e8f6e64a41c8a38e01e314386b001ba
Daniel <danielbatatarpg@hotmail.com>
2022-10-04 20:01:19 -0400

    scripts concertados

--- Assets/Project Assets/Scripts/MainMenuButton.cs
+++ Assets/Project Assets/Scripts/MainMenuButton.cs
@@ -9,7 +9,7 @@ public class MainMenuButton : MonoBehaviour
     // Start is called before the first frame update
     public void Restart()
     {
-        SceneManager.LoadScene("Daniel");
+        SceneManager.LoadScene("Jogo Teste");
     }
 
     public void MainMenuBut()
